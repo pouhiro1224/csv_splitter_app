4f20a0e6a946b5344de4f0184cf61fe020177966
Hiroshi Take <htake.wings@gmail.com>
2024-07-07 15:31:01 +0900

    処理中はプログレスバーと一緒にクルクルを表示して、処理中だとわかりやすいように修正

--- lib/main.dart
+++ lib/main.dart
@@ -76,79 +76,101 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> _splitFile() async {
     setState(() {
       _value = 0;
-      executing=true;
+      executing = true;
     });
+
     if (file == null) {
       print('ファイルが選択されていません。');
       setState(() {
-        executing=false;
+        executing = false;
+      });
+      return;
+    }
+
+    if (!file!.existsSync()) {
+      print('指定したファイルが存在しません。');
+      setState(() {
+        executing = false;
       });
       return;
     }
 
     int linesPerFile = rowsPerFile;
-    final inputFile = file;
-    if (!inputFile.existsSync()) {
-      print('指定したファイルが存在しません。');
-      return;
-    }
-
     int fileCount = 0;
     int linesWritten = 0;
     int allLines = 0;
     int allFileCount = 0;
-
-    late File outputFile;
-    late IOSink outputSink;
-
-    var inputLines = inputFile.openRead();
-    await for (var line in inputLines.transform(Utf8Decoder()).transform(LineSplitter())) {
-      allLines++;//ファイルの行数をカウント
-    }
-    allFileCount = (allLines / linesPerFile).ceil();
-
-    fileCount++;
-    String outputFilePath = _directoryPath! + '/output_$fileCount.csv';
-    outputFile = File(outputFilePath);
-    outputSink = outputFile.openWrite();
     String headerRow = "";
+    IOSink? outputSink;
 
-    inputLines = inputFile.openRead();
-    await for (var line in inputLines.transform(Utf8Decoder()).transform(LineSplitter())) {
-      if(headerRow == ""){
-        //最初、ヘッダ行を取っておいて、書き込んでおく
-        headerRow = line;
-        outputSink.writeln(headerRow);
-        continue;
-      }
-      if (linesWritten != 0 && linesWritten % linesPerFile == 0) {
-        if (outputSink != null) {
-          await outputSink.close(); // ファイルを閉じるまで待機
-          setState(() {
-            _value = fileCount/ allFileCount;
-          });
+    try {
+      // 全行数をカウント
+      allLines = await _countLines(file!);
+      allFileCount = (allLines / linesPerFile).ceil();
+
+      // 入力ファイルを再度開く
+      Stream<String> inputLines = file!.openRead().transform(utf8.decoder).transform(LineSplitter());
+
+      await for (var line in inputLines) {
+        if (headerRow.isEmpty) {
+          // ヘッダー行を取得
+          headerRow = line;
+          continue;
         }
-        fileCount++;
-        outputFilePath = _directoryPath! + '/output_$fileCount.csv';
-        outputFile = File(outputFilePath);
-        outputSink = outputFile.openWrite();
-        //2ファイル目以降は、ヘッダ行を最初に書き込む
-        outputSink.writeln(headerRow);
+
+        if (linesWritten % linesPerFile == 0) {
+          if (linesWritten != 0) {
+            await _closeSink(outputSink); // ファイルを閉じる
+            setState(() {
+              _value = fileCount / allFileCount;
+            });
+          }
+          fileCount++;
+          outputSink = await _createNewOutputSink(fileCount, headerRow);
+        }
+
+        outputSink!.writeln(line);
+        linesWritten++;
       }
 
-      outputSink.writeln(line);
-      linesWritten++;
-    }
+      if (outputSink != null) {
+        await _closeSink(outputSink); // 最後のファイルを閉じる
+      }
 
-    if (outputSink != null) {
-      await outputSink.close(); // 最後のファイルを閉じるまで待機
+      setState(() {
+        _value = 1;
+        executing = false;
+      });
+      print('ファイルの分割が完了しました。');
+    } catch (e) {
+      print('エラーが発生しました: $e');
+      setState(() {
+        executing = false;
+      });
     }
+  }
 
-    setState(() {
-      _value = 1;
-      executing=false;
-    });
-    print('ファイルの分割が完了しました。');
+  Future<int> _countLines(File file) async {
+    int count = 0;
+    await for (var _ in file.openRead().transform(utf8.decoder).transform(LineSplitter())) {
+      count++;
+    }
+    return count;
+  }
+
+  Future<IOSink> _createNewOutputSink(int fileCount, String headerRow) async {
+    String outputFilePath = '$_directoryPath/output_$fileCount.csv';
+    File outputFile = File(outputFilePath);
+    IOSink sink = outputFile.openWrite();
+    sink.writeln(headerRow);
+    return sink;
+  }
+
+  Future<void> _closeSink(IOSink? sink) async {
+    if (sink != null) {
+      await sink.flush();
+      await sink.close();
+    }
   }
 
   @override
@@ -164,85 +186,96 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Card(
-                elevation: 4.0,
-                margin: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "使い方",
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 8),
-                      Text("分割ファイル、分割後ファイルを出力するディレクトリ、分割する行数を入力したら、分割実行ボタンを押してください。"),
-                      Text("出力されるファイルはoutput_{ファイル番号}.csv で出力されます。"),
-                    ],
-                  ),
+              if (executing) ...[
+                CircularProgressIndicator(), // ローディングインジケーターを追加
+                SizedBox(height: 16),
+                LinearProgressIndicator(
+                  minHeight: 20,
+                  value: _value,
                 ),
-              ),
-              Card(
-                elevation: 4.0,
-                margin: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: <Widget>[
-                      ElevatedButton(
-                        onPressed: executing ? null : _pickFileIsSuccess,
-                        child: Text("分割ファイル選択"),
-                      ),
-                      SizedBox(height: 8),
-                      Text("分割するファイル：$fileName"),
-                      SizedBox(height: 16),
-                      ElevatedButton(
-                        onPressed: executing ? null : _selectFolder,
-                        child: Text("出力ディレクトリ選択"),
-                      ),
-                      SizedBox(height: 8),
-                      Text("出力するディレクトリ：${_directoryPath ?? ""}"),
-                      SizedBox(height: 16),
-                      SizedBox(
-                        width: 300,
-                        child: TextField(
-                          enabled: !executing,
-                          keyboardType: TextInputType.number,
-                          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                          decoration: InputDecoration(
-                            labelText: '分割する行数を入力してください',
-                            border: OutlineInputBorder(),
-                          ),
-                          onChanged: (text) {
-                            setState(() {
-                              if (text.isEmpty) {
-                                rowsPerFile = 0;
-                                return;
-                              }
-                              rowsPerFile = int.parse(text);
-                            });
-                          },
+                SizedBox(height: 8),
+                Text(_value == 1 ? "COMPLETE!!" : "処理中..."),
+              ] else ...[
+                Card(
+                  elevation: 4.0,
+                  margin: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "使い方",
+                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                         ),
-                      ),
-                      SizedBox(height: 16),
-                      LinearProgressIndicator(
-                        minHeight: 20,
-                        value: _value,
-                      ),
-                      SizedBox(height: 8),
-                      Text(_value == 1 ? "COMPLETE!!" : ""),
-                      SizedBox(height: 16),
-                      ElevatedButton(
-                        onPressed: (executing || fileName.isEmpty || _directoryPath == null || rowsPerFile == 0)
-                            ? null
-                            : _splitFile,
-                        child: Text("分割実行"),
-                      ),
-                    ],
+                        SizedBox(height: 8),
+                        Text("分割ファイル、分割後ファイルを出力するディレクトリ、分割する行数を入力したら、分割実行ボタンを押してください。"),
+                        Text("出力されるファイルはoutput_{ファイル番号}.csv で出力されます。"),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+                Card(
+                  elevation: 4.0,
+                  margin: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: <Widget>[
+                        ElevatedButton(
+                          onPressed: executing ? null : _pickFileIsSuccess,
+                          child: Text("分割ファイル選択"),
+                        ),
+                        SizedBox(height: 8),
+                        Text("分割するファイル：$fileName"),
+                        SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: executing ? null : _selectFolder,
+                          child: Text("出力ディレクトリ選択"),
+                        ),
+                        SizedBox(height: 8),
+                        Text("出力するディレクトリ：${_directoryPath ?? ""}"),
+                        SizedBox(height: 16),
+                        SizedBox(
+                          width: 300,
+                          child: TextField(
+                            enabled: !executing,
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                            decoration: InputDecoration(
+                              labelText: '分割する行数を入力してください',
+                              border: OutlineInputBorder(),
+                            ),
+                            onChanged: (text) {
+                              setState(() {
+                                if (text.isEmpty) {
+                                  rowsPerFile = 0;
+                                  return;
+                                }
+                                rowsPerFile = int.parse(text);
+                              });
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        LinearProgressIndicator(
+                          minHeight: 20,
+                          value: _value,
+                        ),
+                        SizedBox(height: 8),
+                        Text(_value == 1 ? "COMPLETE!!" : ""),
+                        SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: (executing || fileName.isEmpty || _directoryPath == null || rowsPerFile == 0)
+                              ? null
+                              : _splitFile,
+                          child: Text("分割実行"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ],
           ),
         ),
